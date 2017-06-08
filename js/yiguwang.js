7c8462dbf3e803225ff8427064c756dc414ec5fe
ZBY888 <zhouboyi@163.com>
2017-06-08 21:20:01 +0800

    购物车、登录、注册静态网页跟新完成

--- js/yiguwang.js
+++ js/yiguwang.js
@@ -6,5 +6,37 @@ $(function(){
 //	});
 	
 	$("#header").load("/yiguwang/html/header.html");
+	$("#footer").load("/yiguwang/html/footer.html");
+});
+
+
+
+//index首页轮播flash
+
+$(function(){
+	var img = $(".img_flash ul li")
+	var len = img.length;
+	var outindex = 0;
+	var inindex = 1;
+	var timer;
+	
+	timer = setInterval(time,2000);
+	function time(){
+		$(img).eq(outindex).animate({width:"0",height:"0"},2000).fadeOut().end()
+				.eq(inindex).animate({width:"100%",height:"480px"},1000).fadeIn();
+		outindex = inindex;
+		inindex++;
+		if(inindex == len){
+			inindex = 0;
+		}
+		
+		
+	}
+	
+	
+	
+//	console.log(len)
+	
+	
 });
 
